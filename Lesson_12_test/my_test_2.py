cfa963cfcbccbf3c5ce60a4189982fc49e91a9c9
Vladimir K <iceq3004@yandex.ru>
2024-07-04 09:45:26 +0300

    Update my_test_2.py

--- Lesson_12_test/my_test_2.py
+++ Lesson_12_test/my_test_2.py
@@ -8,8 +8,7 @@ class TestRepeat:
     @pytest.mark.repeat(6)
     def test_function(self):
         test_list = generate_list(10)
-        list_sort(test_list) == sorted(test_list)
-        assert True
+        assert list_sort(test_list) == sorted(test_list)
 
 
 if __name__ == '__main__':
