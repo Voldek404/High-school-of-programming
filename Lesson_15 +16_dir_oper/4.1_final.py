c2e0251662ab5238cd2e83248c8b752cc330bd08
Vladimir K <iceq3004@yandex.ru>
2024-07-04 20:09:57 +0300

    Update 4.1_final.py

--- Lesson_15 +16_dir_oper/4.1_final.py
+++ Lesson_15 +16_dir_oper/4.1_final.py
@@ -1,23 +1,25 @@
 import os
 
 
-def get_list(path, extention, flag):
+def get_list(path, extention, flag, count):
     if flag:
         list_1 = []  # создаем список для имен файлов
-        list_2 = []  # создаем список для имен каталогов
+        list_3 = []
         for f in os.listdir(path):
-            if os.path.isdir(os.path.join(path, f)):
-                list_2.append(f)
-
-            else:
-                if  f.endswith(extention):
+            if os.path.isfile(os.path.join(path, f)):
+                if f.endswith(extention):
                     list_1.append(f)
-        return [list_1, list_2]
+            elif os.path.isdir(os.path.join(path, f)) and count < 2:
+                list_3.append(f)
+                list_2 = get_list(os.path.join(path, f), extention, True, count + 1)
+                for item in list_2:
+                    list_1.append(item)
+        return list_1, list_3
 
 
 path = os.getcwd()
 extention = '.odt'
 flag = True
+count = 0
 
-print(get_list(path, extention, flag))
-
+print(get_list(path, extention, flag, count))
