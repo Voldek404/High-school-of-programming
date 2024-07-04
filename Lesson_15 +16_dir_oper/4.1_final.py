e64ded690840533188c13283d6f3d4ce2d539eb9
Vladimir K <iceq3004@yandex.ru>
2024-07-04 17:59:43 +0300

    Update 4.1_final.py

--- Lesson_15 +16_dir_oper/4.1_final.py
+++ Lesson_15 +16_dir_oper/4.1_final.py
@@ -1,18 +1,23 @@
 import os
 
 
-def get_list(path):
-    list_1 = []  # создаем список для имен файлов
-    list_2 = []  # создаем список для имен каталогов
-    for f in os.listdir(path):
-        if os.path.isdir(os.path.join(path, f)):
-            list_2.append(f)
-        else:
-            if f.endswith('.py'):
-                list_1.append(f)
-    return [list_1, list_2]
+def get_list(path, extention, flag):
+    if flag:
+        list_1 = []  # создаем список для имен файлов
+        list_2 = []  # создаем список для имен каталогов
+        for f in os.listdir(path):
+            if os.path.isdir(os.path.join(path, f)):
+                list_2.append(f)
+
+            else:
+                if  f.endswith(extention):
+                    list_1.append(f)
+        return [list_1, list_2]
 
 
 path = os.getcwd()
+extention = '.odt'
+flag = True
 
-print(get_list(path))
+print(get_list(path, extention, flag))
+
