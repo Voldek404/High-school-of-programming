0014b71208cd7848a736352f6380459a311d569a
Vladimir K <iceq3004@yandex.ru>
2024-07-04 20:53:25 +0300

    Update 4.1_final.py

--- Lesson_15 +16_dir_oper/4.1_final.py
+++ Lesson_15 +16_dir_oper/4.1_final.py
@@ -1,20 +1,21 @@
 import os
 
-
 def get_list(path, extention, flag, count):
-    if flag:
-        list_1 = []  # создаем список для имен файлов
-        list_3 = []
-        for f in os.listdir(path):
-            if os.path.isfile(os.path.join(path, f)):
-                if f.endswith(extention):
-                    list_1.append(f)
-            elif os.path.isdir(os.path.join(path, f)) and count < 2:
-                list_3.append(f)
-                list_2 = get_list(os.path.join(path, f), extention, True, count + 1)
-                for item in list_2:
+    list_1 = []  # создаем список для имен файлов
+    list_3 = []
+    for f in os.listdir(path):
+         if os.path.isfile(os.path.join(path, f)):
+             if f.endswith(extention):
+                list_1.append(f)
+         elif os.path.isdir(os.path.join(path, f)) and count < 2:
+            list_3.append(f)  # добавляем элемент в list_3
+            if flag:
+                results = get_list(os.path.join(path, f), extention, True, count + 1)
+                for item in results:
                     list_1.append(item)
-        return list_1, list_3
+            else:
+                pass
+    return [list_1,list_3]
 
 
 path = os.getcwd()
@@ -22,4 +23,4 @@ extention = '.odt'
 flag = True
 count = 0
 
-print(get_list(path, extention, flag, count))
+print(get_list(path, extention, flag, count)[0])  
