ab8f8e3287e03e460155324568f8d010a7431b3a
Vladimir K <iceq3004@yandex.ru>
2024-07-25 16:38:48 +0300

    Update 28.1.py

--- 28/28.1.py
+++ 28/28.1.py
@@ -1,11 +1,11 @@
-def int_squirrel(int_N):
+def squirrel(N):
     factorial = 1
-    for i in range(1, int_N + 1):
+    for i in range(1, N + 1):
         factorial *= i  # calculating factorial
     first_digit = factorial // 10 ** (len(str(factorial)) - 1)  # dividing factorial
     return first_digit
 
 
-int_N = 5
-print(int_squirrel(int_N))
+N = 5
+print(squirrel(N))
 
