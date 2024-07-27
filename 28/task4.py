af5e4e375ba365975c9c065dea3d092baec14f84
Vladimir K <iceq3004@yandex.ru>
2024-07-27 13:23:55 +0300

    Update task4.py

--- 28/task4.py
+++ 28/task4.py
@@ -1,6 +1,7 @@
 def MadMax(N, list_proto):
-    list_max = [max(list_proto)]
-    list_proto.remove(max(list_proto))
-    list_left = sorted(list_proto[: int(N / 2)])
-    list_right = sorted(list_proto[int(N / 2):], key=None, reverse=True)
+    list_prom = sorted(list_proto)
+    list_max = [max(list_prom)]
+    list_prom.remove(max(list_proto))
+    list_left = list_prom[: int(N / 2)]
+    list_right = sorted(list_prom[int(N / 2):], key=None, reverse=True)
     return list_left + list_max + list_right
