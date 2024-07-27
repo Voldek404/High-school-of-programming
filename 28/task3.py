5a4d1f4558bf2aec375190264415b31581178e16
Vladimir K <iceq3004@yandex.ru>
2024-07-27 11:34:10 +0300

    Update task3.py

--- 28/task3.py
+++ 28/task3.py
@@ -6,12 +6,12 @@ def ConquestCampaign(N, M, L, battalion):
     for i in range(1, N + 1):
         a1 = []
         for j in range(1, M + 1):
-            a1.append('0')
+            a1.append(0)
         training_space.append(a1)
-        
+
     for i in range(L):  # forming first day landing coordinates
-        battalion.append(random.randint(1,N))
-        battalion.append(random.randint(1,M))
+        battalion.append(random.randint(1, N))
+        battalion.append(random.randint(1, M))
 
     for i in range(0, L * 2, 2):  # paint first day points
         x = battalion[i] - 1
@@ -21,34 +21,26 @@ def ConquestCampaign(N, M, L, battalion):
     day = 1
 
     while True:
-        done = False
+        covered = False
 
         for i in range(N):
-            for j in range(M):
-                if training_space[i][j] == day:  # setting moving frames according to range from edges
+            for j in range(M): #set moving to new points and paint it according to day number
+                if training_space[i][j] == day:
                     if i > 0 and training_space[i - 1][j] == 0:
                         training_space[i - 1][j] = day + 1
-                        training_space[i][j] = 1
-                        done = True
+                        covered = True
                     if i < N - 1 and training_space[i + 1][j] == 0:
                         training_space[i + 1][j] = day + 1
-                        training_space[i][j] = 1
-                        done = True
+                        covered = True
                     if j > 0 and training_space[i][j - 1] == 0:
                         training_space[i][j - 1] = day + 1
-                        training_space[i][j] = 1
-                        done = True
+                        covered = True
                     if j < M - 1 and training_space[i][j + 1] == 0:
                         training_space[i][j + 1] = day + 1
-                        training_space[i][j] = 1
-                        done = True
-        
+                        covered = True
 
-        if not done:
+        if not covered:
             break
-    day += 1
 
-            
+        day += 1
 
-
-    return day
