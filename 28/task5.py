c1f702bec69c36944916cf8bbcb96d943d52a960
Vladimir K <iceq3004@yandex.ru>
2024-07-28 14:32:53 +0300

    Update task5.py

--- 28/task5.py
+++ 28/task5.py
@@ -1,8 +1,9 @@
 def SynchronizingTables(N, ids, salary):
     sorted_ids = sorted(ids)
     sorted_salary = sorted(salary)
-
-    ids_salary_dict = {id: salary for id, salary in zip(sorted_ids, sorted_salary)}
-    new_salary = [id_salary_map[id] for id in ids]
+    new_salary = []
+    ids_salary_dict = dict(zip(sorted_ids, sorted_salary))
+    for i in ids:
+        new_salary.append(ids_salary_dict[i])
 
     return new_salary
