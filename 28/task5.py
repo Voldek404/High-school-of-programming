b062bce38c531e0f5ab8837b1bdc54d091277820
Vladimir K <iceq3004@yandex.ru>
2024-07-28 14:09:03 +0300

    Update task5.py

--- 28/task5.py
+++ 28/task5.py
@@ -1,12 +1,8 @@
 def SynchronizingTables(N, ids, salary):
-    sorted_indices_ids = sorted(range(N), key=lambda i: (ids[i], i))
-    sorted_indices_salary = sorted(range(N), key=lambda i: (salary[i], i))
-    salary_sorted = sorted(salary)
-    salary_new = []
+    sorted_ids = sorted(ids)
+    sorted_salary = sorted(salary)
 
-    for i in range(0,N):
-        for j in range(0,N):
-            if sorted_indices_ids[i] == sorted_indices_salary[j]:
-                salary_new.append(salary[i-1])
+    ids_salary_dict = {id: salary for id, salary in zip(sorted_ids, sorted_salary)}
+    new_salary = [id_salary_map[id] for id in ids]
 
-    return salary_new
+    return new_salary
