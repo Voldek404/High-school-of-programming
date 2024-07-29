d3d410e4a2092f1769860e13cf70f184aed260b3
Vladimir K <iceq3004@yandex.ru>
2024-07-29 11:12:42 +0300

    Create task6.py

---
+++ 28/task6.py
@@ -0,0 +1,16 @@
+def PatternUnlock(N, hits):
+    hits_sum = 0
+    for i in range(N):
+        if abs(hits[i] - hits[i + 1]) == 4 or abs(hits[i] - hits[i + 1]) == 2 or abs(hits[i] - hits[i + 1]) == 7 or abs(
+                hits[i] - hits[i + 1]) == 5:
+            hits_sum += 1.41421
+        elif hits[i] == 6 and hits[i + 1] == 1 or hits[i + 1] == 6 and hits[i] == 1:
+            hits_sum += 1
+        else:
+            hits_sum += 1
+    for i in str(hits_sum):
+        res_hits_sum = str(hits_sum).replace('.', '')
+        if i == 0:
+            res_hits_sum = str(hits_sum).replace('0', '')
+    return res_hits_sum
+
