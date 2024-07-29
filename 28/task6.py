013f5795fed0c121237c127edd24dc671a0d8627
Vladimir K <iceq3004@yandex.ru>
2024-07-29 13:29:24 +0300

    Update task6.py

--- 28/task6.py
+++ 28/task6.py
@@ -3,11 +3,13 @@ def PatternUnlock(N, hits):
     for i in range(N):
         if i + 1 == N:
             break
-        if abs(hits[i] - hits[i + 1]) == 4 or abs(hits[i] - hits[i + 1]) == 2 or abs(hits[i] - hits[i + 1]) == 7 or abs(
+        if hits[i] == 6 and hits[i + 1] == 1 or hits[i + 1] == 6 and hits[i] == 1:
+            hits_sum += 1
+        elif hits[i] == 7 and hits[i + 1] == 3 or hits[i + 1] == 7 and hits[i] == 3:
+            hits_sum += 1
+        elif abs(hits[i] - hits[i + 1]) == 4 or abs(hits[i] - hits[i + 1]) == 2 or abs(hits[i] - hits[i + 1]) == 7 or abs(
                 hits[i] - hits[i + 1]) == 5:
             hits_sum += 2 ** 0.5
-        elif hits[i] == 6 and hits[i + 1] == 1 or hits[i + 1] == 6 and hits[i] == 1:
-            hits_sum += 1
         else:
             hits_sum += 1
     hits_sum = round(hits_sum, 5)
