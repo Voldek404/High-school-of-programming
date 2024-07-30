4be68fec887eb55d7244e959516cb6189676efd2
Vladimir K <iceq3004@yandex.ru>
2024-07-30 12:03:35 +0300

    Update task7.py

--- 28/task7.py
+++ 28/task7.py
@@ -11,25 +11,33 @@ def WordSearch(length, input_string, subs):
             splitted_string.pop(0)
             break
         elif len(splitted_string) == 1 and len(splitted_string[0]) > length:
-            search_list.append(splitted_string[0][:length])
+            search_list.append(splitted_string[0][:length - 1])
             splitted_string[0] = splitted_string[0][length:]
-            splitted_string.pop(0)
+            search_list.append(splitted_string[0][:length - 1] + ' ' * (
+                    length - len(splitted_string[0])))
             break
         elif len(splitted_string[0]) < length and len(splitted_string[0] + splitted_string[1]) > length:
             search_list.append(splitted_string[0] + ' ' * (
                     length - len(splitted_string[0])))
             splitted_string.pop(0)
-        elif len(splitted_string[0] + splitted_string[1] + splitted_string[2]) < length:
+        elif len(splitted_string[0] + splitted_string[1]) == length:
+            search_list.append(splitted_string[0])
+            splitted_string.pop(0)
+        elif len(splitted_string[0] + ' ' + splitted_string[1] + ' ' + splitted_string[2]) == length:
             search_list.append(splitted_string[0] + ' ' + splitted_string[1] + ' ' + splitted_string[2])
             splitted_string.pop(0)
             splitted_string.pop(0)
             splitted_string.pop(0)
-        elif len(splitted_string[0] + splitted_string[1]) == length:
-            search_list.append(splitted_string[0])
+        elif len(splitted_string[0] + ' ' + splitted_string[1] + ' ' + splitted_string[2]) < length:
+            search_list.append(splitted_string[0] + ' ' + splitted_string[1] + ' ' + splitted_string[2] + ' ' * (
+                    length - len(splitted_string[0]) - len(splitted_string[1] - len(splitted_string[2]))))
+            splitted_string.pop(0)
+            splitted_string.pop(0)
             splitted_string.pop(0)
         elif len(splitted_string[0]) > length:
-            search_list.append(splitted_string[0][:length])
-            splitted_string[0] = splitted_string[0][length:]
+            search_list.append(splitted_string[0][:length - 1])
+            splitted_string[1] = splitted_string[0][length:]
+            splitted_string.pop(0)
         elif len(splitted_string[0] + splitted_string[1]) < length:
             search_list.append(splitted_string[0] + ' ' + splitted_string[1] + ' ' * (
                     length - len(splitted_string[0] + splitted_string[1] + ' ')))
@@ -45,6 +53,5 @@ def WordSearch(length, input_string, subs):
             result_string += '1'
         else:
             result_string += '0'
-
+        print(rows)
     return result_string
-
