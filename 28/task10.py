4894a55cc562a0da0da29e1ea18d2b46dbaf1a71
Vladimir K <iceq3004@yandex.ru>
2024-07-31 20:49:31 +0300

    Update task10.py

--- 28/task10.py
+++ 28/task10.py
@@ -1,7 +1,7 @@
 def PrintingCosts(Line):
     toner_sum = 0
-    for i in range(1,len(Line)+1):
-        if sample_dict[i] in sample_dict.values():
+    for i in Line:
+        if i in sample_dict.keys():
             toner_sum += sample_dict[i]
         else:
             toner_sum += 23
