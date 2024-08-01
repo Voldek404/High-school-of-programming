f383ef94fe6136e23b27c24ff13f1032287b54b5
Vladimir K <iceq3004@yandex.ru>
2024-08-01 13:03:56 +0300

    Update task11.py

--- 28/task11.py
+++ 28/task11.py
@@ -5,6 +5,9 @@ def BigMinus(string_1, string_2):
     string_1 = list(string_1)
     string_2 = list(string_2)
     result = ''
+    if len(string_1) == len(string_2) == 1:
+        result = str(abs(int(string_1[0]) - int(string_2[0])))
+    return result
     borrow = 0
     for i in range(len(string_1) - 1, -1, -1):
         d_1 = int(string_1[i])
@@ -19,7 +22,4 @@ def BigMinus(string_1, string_2):
         result = str(diff) + result
 
     result = result.lstrip('0')
-
-    if not result:
-        result = '0'
     return result
