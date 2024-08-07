cf0c74855a480b0f113f15a7a406817fc4bc4dcf
Vladimir K <iceq3004@yandex.ru>
2024-08-07 10:23:50 +0300

    Update task15.py

--- 28/task15.py
+++ 28/task15.py
@@ -1,24 +1,29 @@
-def TankRush(H1: int, W1:int, S1: str, H2:int, W2: int, S2: str) -> bool:
+import re
+
+
+def TankRush(H1: int, W1: int, S1: str, H2: int, W2: int, S2: str) -> bool:
     result = False
     map_1_string = ''
     map_2_string = ''
+    index_list = []
     if H1 < H2:
         result = False
     S1 = S1.split()
     S2 = S2.split()
     for i in range(0, len(S2)):
-        map_2_string +=S2[i]
-    for i in range(0, len(S2)-1):
+        map_2_string += S2[i]
+    for i in range(0, len(S2) - 1):
         for j in range(0, len(S1)):
             if S2[i] in S1[j]:
-                first_index = S1[j].index(S2[i])
-                second_index =int(first_index) + W2
-                map_1_string = ''
-                for k in (S1)[j:j + W2]:
-                    map_1_string += k[int(first_index):int(second_index)]
-                    if map_1_string == map_2_string:
-                        result = True
-                        break
+                index_list = [m.start() for m in re.finditer(S2[i], S1[j])]
+                for m in index_list:
+                    second_index = m + W2
+                    map_1_string = ''
+                    for k in (S1)[j:j + W2]:
+                        map_1_string += k[int(m):int(second_index)]
+                        if map_1_string == map_2_string:
+                            result = True
+                            break
             else:
                 continue
 
