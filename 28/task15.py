e656c43e38b5853ee0cdb35b174754625f5f4f75
Vladimir K <iceq3004@yandex.ru>
2024-08-07 07:51:27 +0300

    Update task15.py

--- 28/task15.py
+++ 28/task15.py
@@ -1,22 +1,24 @@
-def TankRush(map_1, map_2):
-    result = 0
-    if map_1[0] < map_2[0]:
-        result = False
+def TankRush(H1: int, W1:int, S1: str, H2:int, W2: int, S2: str) -> bool:
+    result = False
     map_1_string = ''
     map_2_string = ''
-    for i in range(2, len(map_2)):
-        map_2_string += map_2[i]
-    for i in range(2, len(map_2)):
-        for j in range(2, len(map_1)):
-            if map_2[i] in map_1[j]:
-                first_index = map_1[j].index(map_2[i])
-                second_index = first_index + map_2[1]
+    if H1 < H2:
+        result = False
+    S1 = S1.split()
+    S2 = S2.split()
+    for i in range(0, len(S2)):
+        map_2_string +=S2[i]
+    for i in range(0, len(S2)-1):
+        for j in range(0, len(S1)):
+            if S2[i] in S1[j]:
+                first_index = S1[j].index(S2[i])
+                second_index =int(first_index) + W2
                 map_1_string = ''
-                for k in (map_1)[j:j + map_2[1]]:
-                    map_1_string += k[first_index:second_index]
-                if map_1_string == map_2_string:
-                    result = True
-                    break
+                for k in (S1)[j:j + W2]:
+                    map_1_string += k[int(first_index):int(second_index)]
+                    if map_1_string == map_2_string:
+                        result = True
+                        break
             else:
                 continue
 
