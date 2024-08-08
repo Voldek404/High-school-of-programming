0e2034cc999e549279bc256915b216059a4e12c6
Vladimir K <iceq3004@yandex.ru>
2024-08-08 15:01:34 +0300

    Update task16.py

--- 28/task16.py
+++ 28/task16.py
@@ -1,15 +1,18 @@
-def MaximumDiscount(N: int, price: list[int]) -> int:
+def MaximumDiscount(N, price):
     money_savings = 0
+    flag = 0
     raw_price = sorted(price, reverse=True)
-    if N > 3:
+    if N > 3 and N % 3 == 0:
         flag = True
-    elif N == 3:
-        money_savings = raw_price[N - 1]
+    elif N > 3 and N % 3 != 0:
+        flag = False
     elif N < 3:
         money_savings = 0
     for i in range(2, N, 3):
         if flag:
              money_savings += raw_price[i]
+        elif not flag:
+            money_savings += raw_price[i]
     money_savings_3 = money_savings
 
     return money_savings
