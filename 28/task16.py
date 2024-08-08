ade7abed06742ea2d650430f8d0bbd18244d3097
Vladimir K <iceq3004@yandex.ru>
2024-08-08 15:15:15 +0300

    Update task16.py

--- 28/task16.py
+++ 28/task16.py
@@ -2,7 +2,7 @@ def MaximumDiscount(N, price):
     money_savings = 0
     flag = 0
     raw_price = sorted(price, reverse=True)
-    if N > 3:
+    if N >= 3:
         flag = True
     elif N < 3:
         money_savings = 0
