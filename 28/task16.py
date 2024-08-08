5e287d01d45b0db64f7c10473fa9a629accc58ab
Vladimir K <iceq3004@yandex.ru>
2024-08-08 15:14:59 +0300

    Update task16.py
    collected  non-functional trash

--- 28/task16.py
+++ 28/task16.py
@@ -2,17 +2,12 @@ def MaximumDiscount(N, price):
     money_savings = 0
     flag = 0
     raw_price = sorted(price, reverse=True)
-    if N > 3 and N % 3 == 0:
+    if N > 3:
         flag = True
-    elif N > 3 and N % 3 != 0:
-        flag = False
     elif N < 3:
         money_savings = 0
     for i in range(2, N, 3):
         if flag:
              money_savings += raw_price[i]
-        elif not flag:
-            money_savings += raw_price[i]
-    money_savings_3 = money_savings
 
     return money_savings
