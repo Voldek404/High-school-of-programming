072640a0b93d496b89f781339e5bcf629487e753
Vladimir K <iceq3004@yandex.ru>
2024-08-10 14:45:35 +0300

    Update task18.py

--- 28/task18.py
+++ 28/task18.py
@@ -1,13 +1,17 @@
 def MisterRobot(N, data):
     list_2 = sorted(data)
-    while list_2 != data:
-        for j in range(0, 4):
-            while data[j] > data[j + 1] or data[j + 1] > data[j + 2]:
-                if data[j] > data[j + 1] or data[j + 1] > data[j + 2]:
-                    data[j], data[j + 1], data[j + 2] = data[j + 1], data[j + 2], data[j]
+    iterations = 0
+    while data!= list_2 and iterations < 1000:
+        swapped = False
+        for j in range(0, len(data) - 2):
+            if data[j] > data[j + 1] or data[j + 1] > data[j + 2]:
+                if data[j] > data[j + 1]:
+                    data[j], data[j + 1] = data[j + 1], data[j]
+                    swapped = True
+                if data[j + 1] > data[j + 2]:
+                   data[j + 1], data[j + 2] = data[j + 2], data[j + 1]
+                   swapped = True
+        if not swapped:
+                break
+        iterations += 1
     return data == list_2
-
-
-data = [1, 3, 4, 5, 6, 2, 7]
-N = 7
-print(MisterRobot(N, data))
