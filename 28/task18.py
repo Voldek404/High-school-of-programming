5998c7fef93590700362cae2657374ae2ef6619a
Vladimir K <iceq3004@yandex.ru>
2024-08-11 15:23:43 +0300

    Update task18.py

--- 28/task18.py
+++ 28/task18.py
@@ -1,8 +1,5 @@
 def MisterRobot(N: int, data: list) -> bool:
     list_2 = sorted(data)
-    for i in range(0, N - 1):
-        if data[i + 1] > data[i + 2]:
-            return data == list_2
     while list_2 != data:
         for j in range(0, 4):
             while data[j] > data[j + 1] or data[j + 1] > data[j + 2]:
