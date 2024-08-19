6cbc8a8f9ed4c70da209466b1e608e4e9ae0bf88
Vladimir K <iceq3004@yandex.ru>
2024-08-19 21:20:03 +0300

    Update task25.py
    Current range limits

--- 28/task25.py
+++ 28/task25.py
@@ -2,11 +2,11 @@ def TransformTransform(A: list, N: int) -> bool:
     B = []
     if N <= 1:
         return False
-    for i in range(0, N - 1):
-        for j in range(i, N):  
-            B.append(max(A[i:j + 1])) 
+    for i in range(N):
+        for j in range(N - i):  
+            B.append(max(A[j:j + i + 1])) 
     C = []
-    for i in range(0, len(B) - 1):
-        for j in range(i, len(B)): 
-            C.append(max(B[i:j + 1])) 
-    return sum(C) % 2 == 0  
+    for i in range(len(B)):
+        for j in range(len(B) - i): 
+            C.append(max(B[j:j + i + 1])) 
+    return sum(C) % 2 == 0
