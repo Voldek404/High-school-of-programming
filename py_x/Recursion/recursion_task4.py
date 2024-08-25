eedb880cdee7fa3fd4860cd2cd5ce5a5e9b2d779
Vladimir K <iceq3004@yandex.ru>
2024-08-25 16:08:02 +0300

    Update recursion_task4.py

--- py_x/Recursion/recursion_task4.py
+++ py_x/Recursion/recursion_task4.py
@@ -1,10 +1,11 @@
-def recursion_palindrom(number, i = None):
-    if i is None:
-        i = 0
-    string = str(number)
-    if len( string)//2 <= i:
-        return True
-    if  string[i] !=  string[-i-1]:
-        return False
-    return recursion_palindrom(number,i+1)
+def recursion_palindrome_factory():
+    def recursion_palindrom(number, i):
+        string = str(number)
+        if len(string) // 2 <= i:
+            return True
+        if string[i] != string[-i - 1]:
+            return False
+        return recursion_palindrom(number, i + 1)
+    
+    return lambda number: recursion_palindrom(number, 0)
 
