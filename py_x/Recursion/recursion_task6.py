742c25f7d29d57676790cbfa4cfbe8261cc9397c
Vladimir K <iceq3004@yandex.ru>
2024-08-25 16:18:35 +0300

    Update recursion_task6.py

--- py_x/Recursion/recursion_task6.py
+++ py_x/Recursion/recursion_task6.py
@@ -1,11 +1,13 @@
-def recursion_print_even(list_1, list_2=None, i=None):
-    if list_2 is None:
+def recursion_print_even(list_1, *args, **kwargs):
+    if not args:
         list_2 = []
-    if i is None:
-        i = 0
+    else:
+        list_2 = args[0]
+    i = kwargs.get('i', 0)
     if i >= len(list_1):
         return list_2
-    print(i)
-    return recursion_print_even(list_1, list_2, i + 2)
+    list_2.append(i)
+
+    return recursion_print_even(list_1, list_2, i=i + 2)
 
 
