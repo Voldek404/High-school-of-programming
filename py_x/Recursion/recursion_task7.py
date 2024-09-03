e15125dafd2659b962cc027189a31585a1ffcaae
Vladimir K <iceq3004@yandex.ru>
2024-09-03 15:19:50 +0300

    Update recursion_task7.py

--- py_x/Recursion/recursion_task7.py
+++ py_x/Recursion/recursion_task7.py
@@ -1,16 +1,16 @@
-def recursion_second_max_helper(list, index,max,count):
-    if list[index] > max:
-        max = list[index]
-    if index >= len(list) - 1:
-        list.remove(max)
-        if len(list) == 1:
-            return list[0]
+def recursion_second_max_helper(list, index,max,count, new_list):
+    if new_list[index] > max:
+        max = new_list[index]
+    if index >= len(new_list) - 1:
+        new_list.remove(max)
+        if len(new_list) == 1:
+            return new_list[0]
         count += 1
         if count == 2:
             return max
         max = 0
         index = 0
-    return recursion_second_max_helper(list,index=index + 1,max=max, count=count)
+    return recursion_second_max_helper(list,index=index + 1,max=max, count=count, new_list = new_list)
     
 def recursion_second_max(list):
-    return recursion_second_max_helper(list, 0,0,0)
+    return recursion_second_max_helper(list, 0,0,0,list)
