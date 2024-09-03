afb9d322ce46fb35831ab051c1d7faad16b3ce52
Vladimir K <iceq3004@yandex.ru>
2024-09-03 13:32:00 +0300

    Update recursion_task7.py

--- py_x/Recursion/recursion_task7.py
+++ py_x/Recursion/recursion_task7.py
@@ -1,14 +1,7 @@
-def recursion_second_max(list, **kwargs):
-    if len(set(list)) == 1:
-        return None
-    i = kwargs.get('i', 0)
-    max = kwargs.get('max', 0)
-    count = kwargs.get('count', 0)
-    if count is None:
-        count = 0
-    if list[i] > max:
-        max = list[i]
-    if i >= len(list) - 1:
+def recursion_second_max_helper(list, index,max,count):
+    if list[index] > max:
+        max = list[index]
+    if index >= len(list) - 1:
         list.remove(max)
         if len(list) == 1:
             return list[0]
@@ -16,5 +9,8 @@ def recursion_second_max(list, **kwargs):
         if count == 2:
             return max
         max = 0
-        i = 0
-    return recursion_second_max(list, i=i + 1,max=max, count=count)
+        index = 0
+    return recursion_second_max_helper(list,index=index + 1,max=max, count=count)
+    
+def recursion_second_max(list):
+    return recursion_second_max_helper(list, 0,0,0)
