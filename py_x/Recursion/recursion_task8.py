af5defcd0022f6d0a88dad1e62f1b729a1c0b8d5
Vladimir K <iceq3004@yandex.ru>
2024-09-04 18:37:16 +0300

    Create recursion_task8.py

---
+++ py_x/Recursion/recursion_task8.py
@@ -0,0 +1,29 @@
+import os
+
+
+def catalogueWalker(targetDirectory, arrayOfNames):
+    for item in os.listdir(targetDirectory):
+        fullPath = os.path.join(targetDirectory, item)
+
+        if os.path.isfile(fullPath):
+            if item.endswith(fileExtention):
+                arrayOfNames.append(item)
+
+        elif os.path.isdir(fullPath):
+            catalogueWalker(fullPath, arrayOfNames)
+
+    return arrayOfNames
+
+
+def arrayOfAllFiles(targetDirectory):
+    if not os.path.exists(targetDirectory):
+        raise ValueError("Каталог не существует")
+    if not os.listdir(targetDirectory):
+        raise ValueError("Каталог пуст")
+
+    return catalogueWalker(targetDirectory, [])
+
+
+targetDirectory = os.getcwd()
+fileExtention = ''
+
