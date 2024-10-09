7b6ecd2f4610fbfe6d2aa0fa234f3545b4c54928
Voldek404 <iceq3004@yandex.ru>
2024-10-09 13:17:58 +0300

    correct code

--- py_8tasks/8z_task2.py
+++ py_8tasks/8z_task2.py
@@ -20,7 +20,5 @@ def digital_rain(col: str, transformed_list: list):
     return digital_rain_helper(col, transformed_list, {0: -1}, 0, [], 0)
 
 
-col = "110000011"
 transformed_list = [1 if bit == '1' else -1 for bit in col]
-print(digital_rain(col, transformed_list))
 
