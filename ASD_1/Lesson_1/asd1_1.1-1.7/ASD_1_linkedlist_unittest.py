4d6e78556db64f0d8d3ef9d30b9b1274d0d0cda4
Voldek404 <iceq3004@yandex.ru>
2024-10-22 21:38:13 +0300

    updated names

---
+++ ASD_1/Lesson_1/asd1_1.1-1.7/ASD_1_linkedlist_unittest.py
@@ -0,0 +1,77 @@
+import unittest
+
+from ASD_1_linkedlist import LinkedList, Node
+
+
+class TestLinkedListMethods(unittest.TestCase):
+
+    def setUp(self):
+        self.list = LinkedList()
+
+    def test_insert(self):
+        node1 = Node(1)
+        node2 = Node(2)
+        node3 = Node(3)
+
+        self.list.insert(None, node1)
+        self.assertEqual(self.list.head, node1)
+        self.list.insert(node1, node2)
+        self.assertEqual(self.list.head.next, node2)
+        self.list.insert(node2, node3)
+        self.assertEqual(self.list.head.next.next, node3)
+
+    def test_find_all(self):
+        node1 = Node(1)
+        node2 = Node(2)
+        node3 = Node(1)
+
+        self.list.insert(None, node1)
+        self.list.insert(node1, node2)
+        self.list.insert(node2, node3)
+
+        self.assertEqual(self.list.find_all(1), [1, 1])
+        self.assertEqual(self.list.find_all(2), [2])
+        self.assertEqual(self.list.find_all(3), [])
+
+    def test_delete(self):
+        node1 = Node(1)
+        node2 = Node(2)
+        node3 = Node(1)
+        node4 = Node(3)
+
+        self.list.insert(None, node1)
+        self.list.insert(node1, node2)
+        self.list.insert(node2, node3)
+        self.list.insert(node3, node4)
+
+        self.list.delete(1)  # Удаляем только первое вхождение
+        self.assertEqual(self.list.head.value, 2)
+        self.assertEqual(self.list.head.next.value, 1)
+
+        self.list.delete(1, all=True)  # Удаляем все вхождения
+        self.assertEqual(self.list.head.next.value, 3)
+
+    def test_clean(self):
+        node1 = Node(1)
+        node2 = Node(2)
+        self.list.insert(node1, node2)
+
+        self.list.clean()
+        self.assertEqual(self.list.head, None)
+
+    def test_len(self):
+        node1 = Node(1)
+        node2 = Node(2)
+        node3 = Node(3)
+
+        self.assertEqual(self.list.len(), 0)
+
+        self.list.insert(None, node1)
+        self.list.insert(node1, node2)
+        self.list.insert(node2, node3)
+
+        self.assertEqual(self.list.len(), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
