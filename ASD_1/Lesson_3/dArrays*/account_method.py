adb4cbeefad0086465502b14a782d8cfe97bd0ac
Voldek404 <iceq3004@yandex.ru>
2024-11-03 09:14:32 +0300

    account method + multidimensional d_array

--- ASD_1/Lesson_3/dArrays*/account_method.py
+++ ASD_1/Lesson_3/dArrays*/account_method.py
@@ -2,12 +2,11 @@ import ctypes
 
 
 class DynArray:
-
     def __init__(self):
         self.count = 0
-        self.bank_account = 0
         self.capacity = 16
-        self.array = self.make_array(self.capacity)
+        self.bank_account = 4
+        self.array = self.make_array(self.capacity + self.bank_account)
 
     def __len__(self):
         return self.count
@@ -28,34 +27,38 @@ class DynArray:
         self.capacity = new_capacity
 
     def append(self, itm):
-        if self.count == self.capacity:
-            self.resize(2 * self.capacity)
+        if self.count + self.bank_account >= self.capacity:
+            self.capacity = self.capacity * 2
+            self.resize(self.capacity)
+            self.bank_account = int(self.capacity * 0.25)
         self.array[self.count] = itm
         self.count += 1
-        self.bank_account += 2
+        if self.bank_account > 0:
+            self.bank_account -= 1
 
     def insert(self, i, itm):
         if i < 0 or i > self.count:
             raise IndexError('Index is out of bounds')
-        if self.capacity == self.count:
-            self.resize(self.capacity * 2)
-        if self.count == 0 and i == 0:
-            self.array[0] = itm
+        if self.count + self.bank_account >= self.capacity:
+            self.capacity = self.capacity * 2
+            self.resize(self.capacity)
+            self.bank_account = int(self.capacity * 0.25)
         for j in range(self.count, i, -1):
             self.array[j] = self.array[j - 1]
         self.array[i] = itm
         self.count += 1
-        self.bank_account += 2
-
+        if self.bank_account > 0:
+            self.bank_account -= 1
 
     def delete(self, i):
         if i < 0 or i >= self.count:
             raise IndexError('Index is out of bounds')
-        else:
-            for j in range(i, self.count - 1):
-                self.array[j] = self.array[j+1]
-            self.count -= 1
-            self.bank_account -= 1
-        if self.count < 0.5 * self.capacity:
-            new_capacity = max(int(self.capacity / 1.5), 16)
+        for j in range(i, self.count - 1):
+            self.array[j] = self.array[j + 1]
+        self.array[self.count - 1] = None
+        self.count -= 1
+        if self.count < self.capacity // 4:
+            new_capacity = max(self.capacity // 2, 16)
             self.resize(new_capacity)
+            self.capacity = new_capacity
+            self.bank_account = int(self.capacity * 0.25)
--- ASD_1/Lesson_3/dArrays*/multi_dynArray.py
+++ ASD_1/Lesson_3/dArrays*/multi_dynArray.py
@@ -1,71 +1,41 @@
-import ctypes
+class DynamicMultiArray:
+    def __init__(self, dimensions):
+        self.dimensions = dimensions
+        self.size = self.calculate_size(dimensions)
+        self.array = [0] * self.size
 
-class DynArray:
-    def __init__(self):
-        self.row_counts = 0
-        self.num_cols = 0
-        self.capacity_1 = 16
-        self.capacity_2 = 16
-        self.array = self.make_array(self.capacity_1, self.capacity_2)
+    def calculate_size(self, dimensions):
+        size = 1
+        for dim in dimensions:
+            size *= dim
+        return size
 
-    def make_array(self, new_capacity_1, new_capacity_2) -> ctypes.Array:
-        array = (ctypes.py_object * new_capacity_1)()
-        for i in range(new_capacity_1):
-            array[i] = (ctypes.py_object * new_capacity_2)()
-        return array
+    def get_flat_index(self, indices):
+        if len(indices) != len(self.dimensions):
+            raise IndexError("Количество индексов не совпадает с размерностями массива.")
+        flat_index = 0
+        for i in range(len(indices)):
+            flat_index *= self.dimensions[i]
+            flat_index += indices[i]
+        return flat_index
 
-    def append(self, itm, row_index) -> None:
-        if row_index >= self.row_counts:
-            self.row_counts += 1
-        if self.num_cols >= self.capacity_2:
-            self.resize(self.capacity_1, self.capacity_2 * 2)
-        self.array[row_index][self.num_cols] = itm
-        self.num_cols += 1
+    def set_value(self, indices, value):
+        flat_index = self.get_flat_index(indices)
+        self.array[flat_index] = value
 
-    def insert(self, itm, row_index, col_index):
-        if row_index < 0 or row_index >= self.row_counts or col_index < 0 or col_index > self.num_cols:
-            raise IndexError('Row or column index is out of bounds')
-        if self.num_cols >= self.capacity_2:
-            self.resize(self.capacity_1, self.capacity_2 * 2)
-        for j in range(self.num_cols, col_index, -1):
-            self.array[row_index][j] = self.array[row_index][j - 1]
-        self.array[row_index][col_index] = itm
-        self.num_cols += 1
+    def get_value(self, indices):
+        flat_index = self.get_flat_index(indices)
+        return self.array[flat_index]
 
-    def delete(self, row_index, col_index):
-        if row_index < 0 or row_index >= self.row_counts or col_index < 0 or col_index >= self.num_cols:
-            raise IndexError('Row or column index is out of bounds')
-        for j in range(col_index, self.num_cols - 1):
-            self.array[row_index][j] = self.array[row_index][j + 1]
-        self.array[row_index][self.num_cols - 1] = None
-        self.num_cols -= 1
+    def insert(self, indices, value):
+        flat_index = self.get_flat_index(indices)
+        self.size += 1
+        self.array.append(0)
+        for i in range(self.size - 1, flat_index, -1):
+            self.array[i] = self.array[i - 1]
+        self.array[flat_index] = value
+        if indices[-1] == self.dimensions[-1] - 1:
+            self.dimensions[-1] += 1
 
-    def get_row(self, row_index):
-        if row_index < 0 or row_index >= self.row_counts:
-            raise IndexError('Row index is out of bounds')
-        return [self.array[row_index][j] for j in range(self.num_cols)]
-
-    def resize(self, new_capacity_1, new_capacity_2):
-        new_array = (ctypes.py_object * new_capacity_1)()
-        for i in range(new_capacity_1):
-            new_array[i] = (ctypes.py_object * new_capacity_2)()
-
-        for i in range(self.row_counts):
-            for j in range(self.num_cols):
-                new_array[i][j] = self.array[i][j]
-        self.array = new_array
-        self.capacity_1 = new_capacity_1
-        self.capacity_2 = new_capacity_2
-
-
-
-dyn_array = DynArray()
-dyn_array.append(100, 0)
-print("\nПосле добавления элемента в 0-ю строку:")
-print(dyn_array.get_row(0))
-dyn_array.insert(200, 0, 0)
-print("\nПосле вставки элемента в 0-ю строку на позицию 0:")
-print(dyn_array.get_row(0))
-dyn_array.delete(0, 0)
-print("\nПосле удаления элемента из 0-ой строки на позицию 0:")
-print(dyn_array.get_row(0))
+    def display(self):
+        print(self.array)
