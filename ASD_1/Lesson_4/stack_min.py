745966021854d6305293f21f01cdb7cd3b1244b3
Voldek404 <iceq3004@yandex.ru>
2024-11-04 20:02:01 +0300

    new tasks

---
+++ ASD_1/Lesson_4/stack_min.py
@@ -0,0 +1,27 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def size(self):
+        return len(self.stack)
+
+    def pop(self):
+        value = self.stack.pop()
+        if value == self.min_stack[-1]:
+            self.min_stack.pop()
+        return value
+
+    def push(self, value):
+        self.stack.append(value)
+        if not self.min_stack or value <= self.min_stack[-1]:
+            self.min_stack.append(value)
+
+    def peek(self):
+        if self.size() > 0:
+            return self.stack[-1]
+        else:
+            return None
+
+    def show_minimum(self):
+        return self.min_stack[-1]
