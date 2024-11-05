f18b82a9b72e35e2d58b89dfcd5cae7e82bf0a35
Voldek404 <iceq3004@yandex.ru>
2024-11-05 14:23:51 +0300

    new tasks

---
+++ ASD_1/Lesson_4/stack_postfix.py
@@ -0,0 +1,39 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+        self.operating_stack = []
+
+    def size(self):
+        return len(self.stack)
+
+    def pop(self):
+        return self.stack.pop() if self.size() > 0 else None
+
+    def push(self, value):
+        self.stack.append(value)
+        self.evaluate_operations(value)
+
+    def peek(self):
+        return self.stack[-1] if self.size() > 0 else None
+
+    def evaluate_operations(self, value):
+        if isinstance(value, int):
+            self.operating_stack.append(value)
+        elif value in {'+', '-', '*', '/'}:
+            b = self.operating_stack.pop()
+            a = self.operating_stack.pop()
+            if value == '+':
+                self.operating_stack.append(a + b)
+            elif value == '-':
+                self.operating_stack.append(a - b)
+            elif value == '*':
+                self.operating_stack.append(a * b)
+            elif value == '/' and b != 0:
+                self.operating_stack.append(a / b)
+            else:
+                raise ValueError("Division by zero")
+
+    def get_result(self):
+        return self.operating_stack[-1] if len(self.operating_stack) == 1 else None
+
+
