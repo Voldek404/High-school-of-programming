85eb17018254a804d13debeb440fcd3c21d08d86
Voldek404 <iceq3004@yandex.ru>
2024-11-09 14:46:06 +0300

    new tasks. Lesson is done

--- ASD_1/Lesson_5/queue_methods.py
+++ ASD_1/Lesson_5/queue_methods.py
@@ -1,18 +1,26 @@
 class Queue:
     def __init__(self):
         self.queue = []
-        self.begin = -1
-        self.end = -1
 
     def enqueue(self, item):
         self.queue.append(item)
-        self.end += 1
 
     def dequeue(self):
-        if self.begin == self.end:
-            return None
-        self.begin += 1
-        return self.queue[self.begin]
+        return self.queue.pop(0)
 
     def size(self):
-        return self.end - self.begin
+        return len(self.queue)
+
+    def queueRotation(self, number):
+        number = number % self.size() if self.size > 0 else 0
+        for _ in range(number):
+            self.enqueue(self.dequeue())
+        return None
+
+    def queueReverse(self):
+        temp_list = []
+        for i in range(self.size()):
+            temp_list.append(self.dequeue())
+        for item in reversed(temp_list):
+            self.enqueue(item)
+        return self.queue
