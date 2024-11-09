1fa23eb11725c36b0e3eab192154d615b6a318ff
Voldek404 <iceq3004@yandex.ru>
2024-11-09 14:46:06 +0300

    new tasks. Lesson is done

---
+++ ASD_1/Lesson_5/queueOnTwoStacks.py
@@ -0,0 +1,27 @@
+class Stack:
+    def __init__(self):
+        self.stackIn = []
+        self.stackOut = []
+
+    def size(self):
+        return (len(self.stackIn) + len(self.stackOut))
+
+    def peek(self):
+        if len(self.stackOut) > 0:
+            return self.stackOut[-1]
+        elif len(self.stackIn) > 0:
+            return self.stackIn[0]
+        else:
+            return None
+
+    def enqueue(self, item):
+        self.stackIn.append(item)
+
+    def dequeue(self):
+        if len(self.stackOut) == 0:
+            while len(self.stackIn) > 0:
+                self.stackOut.append(self.stackIn.pop())
+        if len(self.stackOut) > 0:
+            return self.stackOut.pop()
+        else:
+            return None
