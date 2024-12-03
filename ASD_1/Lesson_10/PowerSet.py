4eb666bb1553b1f7a54e8f2fe8b19cdfb96dc555
Vladimir K <iceq3004@yandex.ru>
2024-12-03 19:47:41 +0300

    Update PowerSet.py
    intersction method was  changed

--- ASD_1/Lesson_10/PowerSet.py
+++ ASD_1/Lesson_10/PowerSet.py
@@ -82,8 +82,8 @@ def twoPlusIntersections(main_set, sets: list):
 
 def multipleSetsIntersection(sets: list):
     result = sets[0]
-    for i in sets[1:]:
-        result = result.intersection(i)
+    for i in range(1, len[sets] - 1):
+        result = result.intersection(sets[i])
     return result
 
 
