f7e2448539fa5cf51e7c025494b83d80ac238e7a
Vladimir K <iceq3004@yandex.ru>
2025-02-12 19:26:41 +0300

    Update task_13.sql

--- SQL/task_13.sql
+++ SQL/task_13.sql
@@ -9,3 +9,12 @@ Task 13.
 	UPDATE Contacts
 	SET City = 'Piter', Country = 'Russia'
 	WHERE City = 'Berlin' AND Country = 'Germany'
+
+13.3.3.
+	INSERT INTO Shippers(CompanyName, Phone)
+	VALUES ('SDEK', '88005253535')
+
+	DELETE FROM Shippers
+	WHERE ShipperID >= 3
+
+Удаление производится по признаку ShipperID больше значения перед добавлением новых записей
