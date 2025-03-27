2c2013c3089c4689efe07df1f63708ac8195ebe9
Vladimir K <iceq3004@yandex.ru>
2025-03-27 21:21:27 +0300

    Update task_1_tests.py
    test: full test package is completed

--- ASD_2/Lesson_1/task_1_tests.py
+++ ASD_2/Lesson_1/task_1_tests.py
@@ -31,23 +31,77 @@ class SimpleTreeMethods(unittest.TestCase):
         self.assertIn(test_node_1, tree_node_3.Children)
         self.assertIn(test_node_2, tree_node_2.Children)
 
-        self.assertEqual(test_node_1.Parent, tree_node_3)
-        self.assertEqual(test_node_2.Parent, tree_node_2)
-
-        
-
-    def testDeleteNode(self):
-
     def testGetAllNodes(self):
+        self.assertEqual(self.empty_tree.GetAllNodes(), [])
+
+        node2 = SimpleTreeNode(2, self.root.Root)
+        node3 = SimpleTreeNode(3, self.root.Root)
+        self.root.Root.Children.extend([node2, node3])
+        node4 = SimpleTreeNode(4, node2)
+        node2.Children.append(node4)
+
+        all_nodes = self.root.GetAllNodes()
+        values = [node.NodeValue for node in all_nodes]
+
+        self.assertCountEqual(values, [1, 2, 3, 4])
 
     def testFindNodesByValue(self):
+        self.assertEqual(self.empty_tree.FindNodesByValue(123), [])
+
+        node2 = SimpleTreeNode(5, self.root.Root)
+        node3 = SimpleTreeNode(3, self.root.Root)
+        node4 = SimpleTreeNode(5, node2)
+        self.root.Root.Children.extend([node2, node3])
+        node2.Children.append(node4)
+
+        found_nodes = self.root.FindNodesByValue(5)
+        self.assertEqual(len(found_nodes), 2)
+        for node in found_nodes:
+            self.assertEqual(node.NodeValue, 5)
 
     def testMoveNode(self):
+        try:
+            self.empty_tree.MoveNode(SimpleTreeNode(1, None), None)
+        except Exception as e:
+            self.fail(f"MoveNode failed on empty tree: {e}")
+
+        node2 = SimpleTreeNode(2, self.root.Root)
+        node3 = SimpleTreeNode(3, node2)
+        self.root.Root.Children.append(node2)
+        node2.Children.append(node3)
+
+        self.root.MoveNode(node3, self.root.Root)
+
+        self.assertNotIn(node3, node2.Children)
+
+        self.assertEqual(node3.Parent, self.root.Root)
+
+        self.assertIn(node3, self.root.Root.Children)
 
     def testCount(self):
+        self.assertEqual(self.empty_tree.Count(), 0)
+
+        node2 = SimpleTreeNode(2, self.root.Root)
+        node3 = SimpleTreeNode(3, node2)
+        self.root.Root.Children.append(node2)
+        node2.Children.append(node3)
+
+        self.assertEqual(self.root.Count(), 3)
 
     def testLeafCount(self):
+        self.assertEqual(self.empty_tree.LeafCount(), 0)
 
+        node2 = SimpleTreeNode(2, self.root.Root)
+        node3 = SimpleTreeNode(3, node2)
+        self.root.Root.Children.append(node2)
+        node2.Children.append(node3)
+
+        self.assertEqual(self.root.LeafCount(), 1)
+
+        node4 = SimpleTreeNode(4, self.root.Root)
+        self.root.Root.Children.append(node4)
+
+        self.assertEqual(self.root.LeafCount(), 2)
 
 
 if __name__ == '__main__':
