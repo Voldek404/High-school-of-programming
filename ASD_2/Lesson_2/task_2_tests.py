395beecdf999d2f25199d461b64866723cd128fa
Владимир Корнеев <iceq3004@yandex.ru>
2025-04-02 20:34:01 +0300

    test:DeleteNode_method_re-write

--- ASD_2/Lesson_2/task_2_tests.py
+++ ASD_2/Lesson_2/task_2_tests.py
@@ -22,6 +22,11 @@ class BSTMethods(unittest.TestCase):
         result = self.tree.FindNodeByKey(20)
         self.assertFalse(result.NodeHasKey)
 
+        self.tree.DeleteNodeByKey(7)
+
+        result = self.tree.FindNodeByKey(7)
+        self.assertFalse(result.NodeHasKey)
+
     def testAddKeyValue(self):
         self.assertTrue(self.tree.AddKeyValue(6, "New Node"))
         self.assertFalse(self.tree.AddKeyValue(7, "Duplicate"))
@@ -32,12 +37,35 @@ class BSTMethods(unittest.TestCase):
         self.assertEqual(min_node.NodeKey, 2)
         self.assertEqual(max_node.NodeKey, 17)
 
-    def testDeleteNodeByKey(self):
-        self.assertTrue(self.tree.DeleteNodeByKey(7))
-        result = self.tree.FindNodeByKey(7)
-        self.assertFalse(result.NodeHasKey)
+    def testDeleteLeafNode(self):
+        self.assertTrue(self.tree.FindNodeByKey(17).NodeHasKey)
+        self.tree.DeleteNodeByKey(17)
+        self.assertFalse(self.tree.FindNodeByKey(17).NodeHasKey)
 
-        self.assertFalse(self.tree.DeleteNodeByKey(20))
+    def testDeleteNodeWithOneChild(self):
+        self.tree.AddKeyValue(6, "left-right-left")
+        self.assertTrue(self.tree.FindNodeByKey(7).NodeHasKey)
+        self.tree.DeleteNodeByKey(7)
+        self.assertFalse(self.tree.FindNodeByKey(7).NodeHasKey)
+        self.assertTrue(self.tree.FindNodeByKey(6).NodeHasKey)
+
+    def testDeleteNodeWithTwoChildren(self):
+        self.assertTrue(self.tree.FindNodeByKey(5).NodeHasKey)
+        self.tree.DeleteNodeByKey(5)
+        self.assertFalse(self.tree.FindNodeByKey(5).NodeHasKey)
+        self.assertTrue(self.tree.FindNodeByKey(7).NodeHasKey)  # Проверяем, что новый узел на месте 5
+
+    def testDeleteRootNode(self):
+        self.assertTrue(self.tree.FindNodeByKey(10).NodeHasKey)
+        self.tree.DeleteNodeByKey(10)
+        self.assertFalse(self.tree.FindNodeByKey(10).NodeHasKey)
+        self.assertTrue(self.tree.Root is not None)  # Проверяем, что новый корень установлен
+
+    def testTreeStructureAfterDeletion(self):
+        self.tree.DeleteNodeByKey(5)
+        self.assertFalse(self.tree.FindNodeByKey(5).NodeHasKey)
+        self.assertTrue(self.tree.FindNodeByKey(15).NodeHasKey)
+        self.assertTrue(self.tree.FindNodeByKey(7).NodeHasKey)
 
     def testCount(self):
         self.assertEqual(self.tree.Count(), 7)
