92c02fc6fc8c3c6c18a72c008af4a6abb2c12d08
Vladimir K <iceq3004@yandex.ru>
2025-05-05 07:08:44 +0300

    Update task_8.py
    fix:refactor_according_to_task

--- ASD_2/Lesson_8/task_8.py
+++ ASD_2/Lesson_8/task_8.py
@@ -16,78 +16,24 @@ class SimpleGraph:
                 return True
         return False
 
-    def RemoveVertex(self, v):
-        for i in range(self.max_vertex):
-            if self.vertex[i] is not None:
-                if self.vertex[i].Value == v:
-                    self.vertex[i] = None
-                    target_index = i
-                    break
-        else:
-            return False
-
-        for j in range(self.max_vertex):
-            self.m_adjacency[target_index][j] = 0
-            self.m_adjacency[j][target_index] = 0
-
-        return True
+     def RemoveVertex(self, v):
+        if 0 <= v < self.max_vertex:
+            self.vertex[v] = None
+            for i in range(self.max_vertex):
+                self.m_adjacency[v][i] = 0
+                self.m_adjacency[i][v] = 0
 
     def IsEdge(self, v1, v2):
-        first_index = None
-        second_index = None
-        for i in range(self.max_vertex):
-            if self.vertex[i] is not None and self.vertex[i].Value == v1:
-                first_index = i
-                break
-        for j in range(self.max_vertex):
-            if self.vertex[j] is not None and self.vertex[j].Value == v2:
-                second_index = j
-                break
-
-        if first_index is None or second_index is None:
-            return False
-
-        return (
-            self.m_adjacency[first_index][second_index] == 1
-            or self.m_adjacency[second_index][first_index] == 1
-        )
+        if 0 <= v1 < self.max_vertex and 0 <= v2 < self.max_vertex:
+            return self.m_adjacency[v1][v2] == 1
+        return False
 
     def AddEdge(self, v1, v2):
-        first_index = None
-        second_index = None
-        for i in range(self.max_vertex):
-            if self.vertex[i] is not None and self.vertex[i].Value == v1:
-                first_index = i
-                break
-        for j in range(self.max_vertex):
-            if self.vertex[j] is not None and self.vertex[j].Value == v2:
-                second_index = j
-                break
-
-        if first_index is None or second_index is None:
-            return False
-
-        self.m_adjacency[first_index][second_index] = 1
-        self.m_adjacency[second_index][first_index] = 1
-        return True
+        if 0 <= v1 < self.max_vertex and 0 <= v2 < self.max_vertex:
+            self.m_adjacency[v1][v2] = 1
+            self.m_adjacency[v2][v1] = 1
 
     def RemoveEdge(self, v1, v2):
-        first_index = None
-        second_index = None
-        for i in range(self.max_vertex):
-            if self.vertex[i] is not None and self.vertex[i].Value == v1:
-                first_index = i
-                break
-        for j in range(self.max_vertex):
-            if self.vertex[j] is not None and self.vertex[j].Value == v2:
-                second_index = j
-                break
-
-        if first_index is None or second_index is None:
-            return False
-
-        if self.m_adjacency[first_index][second_index] == 1:
-            self.m_adjacency[first_index][second_index] = 0
-            self.m_adjacency[second_index][first_index] = 0
-            return True
-        return False
+        if 0 <= v1 < self.max_vertex and 0 <= v2 < self.max_vertex:
+            self.m_adjacency[v1][v2] = 0
+            self.m_adjacency[v2][v1] = 0
