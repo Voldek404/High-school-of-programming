77479231e501b60c4f0fcd96caad2a290b390bcc
Vladimir K <iceq3004@yandex.ru>
2025-05-09 21:48:58 +0300

    Update task_9-2.py

--- ASD_2/Lesson_9/task_9-2.py
+++ ASD_2/Lesson_9/task_9-2.py
@@ -29,3 +29,46 @@ class SimpleTree:
         EvenTreesHelper(self.Root)
 
         return result_list
+
+    def collect_values(self, node):
+        values = [node.NodeValue]
+        for child in node.Children:
+            values.extend(self.collect_values(child))
+        return values
+
+    def build_balanced_tree(self, values, parent=None):
+        if not values:
+            return None
+
+        mid_index = len(values) // 2
+        node = SimpleTreeNode(values[mid_index], parent)
+        left = self.build_balanced_tree(values[:mid_index], node)
+        right = self.build_balanced_tree(values[mid_index + 1:], node)
+
+        if left:
+            node.Children.append(left)
+        if right:
+            node.Children.append(right)
+        return node
+
+    def balance_even_tree(self):
+        if self.Root is None:
+            return
+        values = self.collect_values(self.Root)
+        values.sort()
+        self.Root = self.build_balanced_tree(values)
+
+    def count_even_subtrees(self, node):
+        count = 0
+
+        def dfs(current):
+            nonlocal count
+            size = 1
+            for child in current.Children:
+                size += dfs(child)
+            if (size - 1) % 2 == 0 and current.Parent is not None:
+                count += 1
+            return size
+
+        dfs(node)
+        return count
