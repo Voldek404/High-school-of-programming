193b3b3da8ab9b30bf592d7a8c0f13bb096a73c6
Vladimir K <iceq3004@yandex.ru>
2025-05-11 13:00:20 +0300

    Update task_9.py

--- ASD_2/Lesson_9/task_9.py
+++ ASD_2/Lesson_9/task_9.py
@@ -6,8 +6,116 @@ class SimpleTreeNode:
 
 
 class SimpleTree:
-    def __init__(self, root):
+    def __init__(self, root, level):
         self.Root = root
+        self.Level = level
+
+    def updateSubtreeLevel(node, level):
+        node.level = level
+        for child in node.Children:
+            updateSubtreeLevel(child, level + 1)
+
+    def AddChild(self, ParentNode, NewChild):
+        if self.Root is None:
+            self.Root = NewChild
+            return
+
+        def AddChildHelper(currentNode):
+            if currentNode == ParentNode:
+                currentNode.Children.append(NewChild)
+                NewChild.Parent = currentNode
+                return
+            for child in currentNode.Children:
+                AddChildHelper(child)
+
+        return AddChildHelper(self.Root)
+
+    def DeleteNode(self, NodeToDelete):
+        if self.Root is None:
+            return
+        if NodeToDelete == self.Root:
+            self.Root = None
+            return
+
+        def DeleteNodeHelper(currentNode):
+            for child in currentNode.Children:
+                if child == NodeToDelete:
+                    currentNode.Children.remove(child)
+                else:
+                    DeleteNodeHelper(child)
+
+        return DeleteNodeHelper(self.Root)
+
+    def GetAllNodes(self) -> List[SimpleTreeNode]:
+        if self.Root is None:
+            return []
+
+        def GetAllNodesHelper(currentNode, nodesList: list) -> list:
+            nodesList.append(currentNode)
+            if currentNode.Children:
+                for child in currentNode.Children:
+                    GetAllNodesHelper(child, nodesList)
+            return nodesList
+
+        return GetAllNodesHelper(self.Root, [])
+
+    def FindNodesByValue(self, val) -> list:
+        if self.Root is None:
+            return []
+
+        def FindNodesHelper(currentNode, nodesList: list) -> list:
+            if currentNode.NodeValue == val:
+                nodesList.append(currentNode)
+            for child in currentNode.Children:
+                FindNodesHelper(child, nodesList)
+            return nodesList
+
+        return FindNodesHelper(self.Root, [])
+
+    def MoveNode(self, OriginalNode, NewParent):
+        if self.Root is None:
+            return
+
+        def MoveNodeHelper(currentNode):
+            for child in currentNode.Children:
+                if child == OriginalNode:
+                    NewParent.Children.append(OriginalNode)
+                    OriginalNode.level = currentNode.Level + 1
+                    updateSubtreeLevel(OriginalNode, OriginalNode.level)
+                    OriginalNode.Parent = NewParent
+                    currentNode.Children.remove(child)
+                    break
+                else:
+                    MoveNodeHelper(child)
+
+        return MoveNodeHelper(self.Root)
+
+    def Count(self) -> int:
+        if self.Root is None:
+            return 0
+
+        def CountHelper(currentNode)-> int:
+            count = 1
+            if currentNode.Children:
+                for node in currentNode.Children:
+                    count += CountHelper(node)
+            return count
+
+        return CountHelper(self.Root)
+
+    def LeafCount(self) -> int:
+        if self.Root is None:
+            return 0
+
+        def LeafCountHelper(currentNode):
+            count = 0
+            if not currentNode.Children:
+                return 1
+            for child in currentNode.Children:
+                count += LeafCountHelper(child)
+            return count
+
+        return LeafCountHelper(self.Root)
 
     def EvenTrees(self):
         if self.Root is None or len(self.Root.Children) == 0:
