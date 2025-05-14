82b4ef701aebd6995c69b6bf3eb8f545b6d45c0c
Владимир Корнеев <iceq3004@yandex.ru>
2025-05-14 16:06:56 +0300

    code:new_method_done

--- ASD_2/Lesson_10/task_10.py
+++ ASD_2/Lesson_10/task_10.py
@@ -1,6 +1,7 @@
 class Vertex:
     def __init__(self, val):
         self.Value = val
+        self.Hit = False
 
 
 class SimpleGraph:
@@ -14,16 +15,16 @@ class SimpleGraph:
             if self.vertex[i] is None:
                 self.vertex[i] = Vertex(v)
                 return True
-        return False  
+        return False
 
     def RemoveVertex(self, v):
         if 0 <= v < self.max_vertex and self.vertex[v] is not None:
-            self.vertex[v] = None  
+            self.vertex[v] = None
             for i in range(self.max_vertex):
                 self.m_adjacency[v][i] = 0
                 self.m_adjacency[i][v] = 0
             return True
-        return False  
+        return False
 
     def IsEdge(self, v1, v2):
         if 0 <= v1 < self.max_vertex and 0 <= v2 < self.max_vertex:
@@ -45,3 +46,32 @@ class SimpleGraph:
                 self.m_adjacency[v2][v1] = 0
                 return True
         return False
+
+    def DeepFirstSearch(self, VFrom, VTo):
+        curr_stack = []
+
+        def DeepFirstSearchHelper(current_A_vertex):
+            self.vertex[current_A_vertex].Hit = True
+            curr_stack.append(current_A_vertex)
+            if self.IsEdge(current_A_vertex, VTo):
+                curr_stack.append(VTo)
+                return curr_stack
+
+            else:
+                for i in range(self.max_vertex):
+                    if (
+                        not self.vertex[i].Hit
+                        and self.m_adjacency[current_A_vertex][i] == 1
+                    ):
+                        result = DeepFirstSearchHelper(i)
+                        if result:
+                            return result
+
+                curr_stack.pop()
+
+        result = DeepFirstSearchHelper(VFrom)
+        if result:
+            return result
+
+        else:
+            return []
