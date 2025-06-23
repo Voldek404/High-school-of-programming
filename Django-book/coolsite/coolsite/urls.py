2c19febdde9afd1edfa045fe24d2efbfd150dbe6
Владимир Корнеев <iceq3004@yandex.ru>
2025-06-23 12:39:32 +0300

    code:templates_basic_done

--- Django-book/coolsite/coolsite/urls.py
+++ Django-book/coolsite/coolsite/urls.py
@@ -14,17 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
 
-
+from coolsite import settings
 from women.views import *
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('cats/<int:catid>/', categories),
+    path('', include('women.urls')),
 ]
 
 handler404 = pageNotFound
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
