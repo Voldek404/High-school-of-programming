5afefd1b9c0e83c4650f9ab2a7a7ae3a4adf154d
Владимир Корнеев <iceq3004@yandex.ru>
2025-06-28 10:41:23 +0300

    code:new_module

---
+++ Django/store-server/store/products/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+from products.views import products
+
+app_name = "products"
+
+urlpatterns = [path("", products, name="index"),
+]
+
+
+
+
+
