0d945083094ee2840486e0426bf484743f713003
Владимир Корнеев <iceq3004@yandex.ru>
2025-07-18 23:17:31 +0300

    bug:users_profile_is_done

--- Django/store-server/store/users/views.py
+++ Django/store-server/store/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect, redirect
 from django.urls import reverse
-from django.contrib import auth
+from django.contrib import auth, messages
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
@@ -25,6 +25,7 @@ def registration(request):
         form = UserRegistrationForm(data = request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Вы успешно зарегистрированы!')
             return HttpResponseRedirect(reverse('users:login'))
     else:
         form = UserRegistrationForm()
@@ -36,6 +37,7 @@ def profile(request):
         form = UserProfileForm(instance = request.user, data = request.POST, files = request.FILES)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Поздравляем! Вы успешно зарегистрировались')
             return HttpResponseRedirect(reverse('users:profile'))
         else:
             print(form.errors)
@@ -43,3 +45,7 @@ def profile(request):
         form = UserProfileForm(instance=request.user)
     context = {'title': 'Store - Профиль', 'form': form }
     return render(request, 'users/profile.html', context)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
