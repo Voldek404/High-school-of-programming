fc3220f6a845fb049052c578c9b061dbdc722329
Владимир Корнеев <iceq3004@yandex.ru>
2025-07-23 21:59:06 +0300

    code:base_level_done

--- Django/store-server/store/users/admin.py
+++ Django/store-server/store/users/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from users.models import User
+from products.admin import BasketAdmin
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username',)
+    inlines = (BasketAdmin,)
+
