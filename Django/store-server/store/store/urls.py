a2871d52315cbb501a2ba44d1c196635f7324a09
Владимир Корнеев <iceq3004@yandex.ru>
2025-07-26 22:30:50 +0300

    code:cbv_products_done

--- Django/store-server/store/store/urls.py
+++ Django/store-server/store/store/urls.py
@@ -19,14 +19,13 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from products.views import index
-from products.views import products
+from products.views import IndexView
 from users.views import login, registration
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', IndexView.as_view(extra_context = {'title':'Store'}), name='index'),
     path('products/', include('products.urls', namespace='products')),
     path('users/', include('users.urls', namespace='users')),
 ]
