3f3672b27e2c72e14f5d85c720374088174c0667
Владимир Корнеев <iceq3004@yandex.ru>
2025-07-30 22:44:36 +0300

    code:sqllite_to_postgres_done

--- Django/store-server/store/products/urls.py
+++ Django/store-server/store/products/urls.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 from products.views import ProductsListView, basket_add, basket_remove
 
