de4fb85e83d2eaabdff1b0f2e21529b13503be4d
Владимир Корнеев <iceq3004@yandex.ru>
2025-08-03 22:06:37 +0300

    code:renew_women_model

--- Django-book/coolsite/women/views.py
+++ Django-book/coolsite/women/views.py
@@ -1,13 +1,28 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import redirect, render
 from .models import *
+from django.urls import reverse
 
-menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
+menu = menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'add_page'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+]
 
 
 def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
+    cats = Category.objects.all()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Главная страница',
+        'cat_selected': 0,
+    }
+
+    return render(request, 'women/index.html', context=context)
 
 
 def about(request):
@@ -26,3 +41,42 @@ def archive(request, year):
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+
+def about(request):
+    return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
+
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+def get_absolute_url(self):
+    return reverse('post', kwargs={'post_id': self.pk})
+
+
+def show_category(request, cat_id):
+    posts = Women.objects.filter(cat_id=cat_id)
+    if len(posts) == 0:
+        raise Http404()
+    cats = Category.objects.all()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Главная страница',
+        'cat_selected': cat_id,
+    }
+
+    return render(request, 'women/index.html', context=context)
