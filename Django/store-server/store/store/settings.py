dcea6a7ef21d14f0773209f118648454a0ede1ea
Владимир Корнеев <iceq3004@yandex.ru>
2025-08-09 16:52:49 +0300

    code:settings_py_modified

--- Django/store-server/store/store/settings.py
+++ Django/store-server/store/store/settings.py
@@ -175,6 +175,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (BASE_DIR / 'static',)
 
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / 'static'
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
